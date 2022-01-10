0e8e88cabd08133c9f3c56bc558fb749a33f65c0
cuneytbatirel <44145700+cuneytbatirel@users.noreply.github.com>
2022-01-10 23:00:15 +0300

    Fixed errors and extended functionality

--- test/load_test/k6.js
+++ test/load_test/k6.js
@@ -1,63 +1,123 @@
+// K6 LOAD TEST
+
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
+// Load test options
 export const options = {
   stages: [
-    { duration: '10m', target: 1000 }, // Ramp-up of traffic from 1 to 1000 users over 10 minutes.
-    { duration: '20m', target: 1000 }, // Stay at 1000 users for 20 minutes
-    { duration: '10m', target: 0 }, // Ramp-down to 0 users
+    { duration: '1m', target: 50 }, // Ramp-up of traffic from 1 to 50 users over 1 minute.
+    { duration: '2m', target: 50 }, // Stay at 50 users for 2 minutes
+    { duration: '1m', target: 0 }, // Ramp-down to 0 users
   ],
+  // Thresholds to be checked
   thresholds: {
-    'http_req_duration': ['p(95) < 500'], // 95% of requests must complete below 500ms
-    'login successful': ['p(95) < 500'],
-    'qr creation successful': ['p(95) < 500'],
-    'get qr successful': ['p(95) < 500'],
-    'get qr image successful': ['p(95) < 500'],
-    'qr update successful': ['p(95) < 500'],
-    'qr deletion successful': ['p(95) < 500'],
-    'get qr stats successful': ['p(95) < 500'],
+    'http_req_duration': ['p(95) < 2000'], // 95% of requests must complete below 2s
+    'Login successful': ['p(95) < 500'], // 95% of requests must complete below 0.5s
+    'QR creation successful': ['p(95) < 500'],
+    'Get QR successful': ['p(95) < 500'],
+    'Get QR image successful': ['p(95) < 500'],
+    'QR update successful': ['p(95) < 500'],
+    'QR deletion successful': ['p(95) < 500'],
+    'Get QR stats successful': ['p(95) < 500'],
   },
 };
 
-const BASE_URL = 'http://localhost:8000/docs#/default';
-const USERNAME = 'user';
-const PASSWORD = 'pass';
-const LINK = 'link';
+// Shortcuts to be used in links
+const URL = 'http://localhost:8000';
 const NEWLINK = 'newlink';
 const ID = '1';
-const VERSION = '1';
-const SIZE = '1024';
+const ID2 = '2';
+const V = '1';
+const SIZE = '512';
 
+// Default function
 export default () => {
 
-  const login = http.post(`${BASE_URL}/login_login_post/`, { username: USERNAME, password: PASSWORD, });
+  let cred = { username: 'user', password: 'pass', };
+  let qr_link = { link: 'link', };
+  let img = { qr_id: '2', version: '1', size: '512', };
+  let id = { qr_id: '1', };
+  let update = { qr_id: 2, new_link: 'newlink', };
+  
+  const params = {
+    headers: {
+    'accept': 'application/json',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Authorization': 'Bearer 2',
+    },
+  };
+  
+  const multi = {
+    headers: {
+    'accept': 'application/json',
+    // 'Content-Type': 'application/x-www-form-urlencoded',
+    'Authorization': 'Bearer 2',
+    },
+  };
+  
+  // Login post function - POST
 
-  check(login, { 'login successful': (res1) => res1.json('access') !== '', });
+  const login = http.post(`${URL}/login`, cred,);
+
+  check(login, {
+  'Login successful': (res1) => res1.json('access') !== '',
+  '↑ Login Response 200 ↑': (r1) => r1.status === 200,
+  });
   
-  const create_qr = http.post(`${BASE_URL}/create_qr_create_qr_post`, { link: LINK, });
+  // Create qr function - POST
   
-  check(create_qr, {'qr creation successful': (res2) => res2.json('link') !== '',});
+  const create_qr = http.post(`${URL}/create_qr`, qr_link, params);
   
-  const get_qr = http.get(`${BASE_URL}/my_qr_codes_my_qr_codes_get`);
+  check(create_qr, {
+  'QR creation successful': (res2) => res2.json('link') !== '',
+  '↑ Create Response 200 ↑': (r2) => r2.status === 200,
+  });
   
-  check(get_qr, {'get qr successful': (res3) => res3.json('id', 'link') !== '',});
+  // Get qr function - GET
   
-  const get_image = http.post(`${BASE_URL}/get_qr_img_get_qr_img_post`,
-  { qr_id: ID, version: VERSION, size: SIZE, });
+  const get_qr = http.get(`${URL}/my_qr_codes`, params);
   
-  check(get_qr, {'get qr image successful': (res4) => res4.json('image') !== '',});
+  check(get_qr, {
+  'Get QR successful': (res3) => res3.json('id', 'link') !== '',
+  '↑ Get QR Response 200 ↑': (r3) => r3.status === 200,
+  });
   
-  const update_qr = http.post(`${BASE_URL}/update_qr_update_qr_post`, { link: NEWLINK, });
+  // Get image function - POST
   
-  check(update_qr, {'qr update successful': (res5) => res5.json('newlink') !== '',});
+  const get_image = http.post(`${URL}/get_qr_img?qr_id=${ID2}`, {}, multi);
   
-  const delete_qr = http.post(`${BASE_URL}/delete_qr_delete_qr_post`, { qr_id: ID, });
+  check(get_image, {
+  'Get QR image successful': (res4) => res4.json('image') !== '',
+  '↑ Get Image Response 200 ↑': (r4) => r4.status === 200,
+  });
   
-  check(delete_qr, {'qr deletion successful': (res6) => res6.json('deleted') !== '',});
+  // Update qr function - POST
   
-  const get_stats = http.get(`${BASE_URL}/get_qr_stats_get_qr_stats_get`, { qr_id: ID, });
+  const update_qr = http.post(`${URL}/update_qr?qr_id=${ID2}&new_link=${NEWLINK}`, update, params);
   
-  check(get_stats, {'get qr stats successful': (res7) => res7.json('stats') !== '',});
+  check(update_qr, {
+  'QR update successful': (res5) => res5.json('newlink') !== '',
+  '↑ Update Response 200 ↑': (r5) => r5.status === 200,
+  });
+  
+  // Delete qr function - POST
+  
+  const delete_qr = http.post(`${URL}/delete_qr?qr_id=${ID}`, id, params);
+  
+  check(delete_qr, {
+  'QR deletion successful': (res6) => res6.json('deleted') !== '',
+  '↑ Delete Response 200 ↑': (r6) => r6.status === 200,
+  });
+  
+  // Get stats function - GET
+  
+  const get_stats = http.get(`${URL}/get_qr_stats?qr_id=${ID}`, params);
+  
+  check(get_stats, {
+  'Get QR stats successful': (res7) => res7.json('stats') !== '',
+  '↑ Stats Response 200 ↑': (r7) => r7.status === 200,
+  });
   
   sleep(1);
 };
