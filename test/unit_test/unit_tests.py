883a4a0c42bc4cccac76c3f49006ff0f2900abb5
BerkayTatoglu <92725049+BerkayTatoglu@users.noreply.github.com>
2022-01-10 17:24:03 +0300

    updated to acquire 100% coverage
    updated to acquire 100% statement and branch coverage

--- test/unit_test/unit_tests.py
+++ test/unit_test/unit_tests.py
@@ -2,7 +2,8 @@ from datetime import datetime
 from PIL import Image
 from utils import hash_sha256, generate_qr, timestamp, get_IP_location
 from data_layer import User
-from logic_layer import login_user, register_user, decode_token, insert_new_qr, get_user_qr, generate_qr_for_user, get_qr_redirect_link, change_qr_link, delete_qr_code,log_qr_access,  get_qr_access, set_user_premium, admin_get_all_users, admin_ban_user, admin_get_all_qr
+from logic_layer import login_user, register_user, decode_token, insert_new_qr, get_user_qr, generate_qr_for_user,\
+     get_qr_redirect_link, change_qr_link, delete_qr_code,log_qr_access,  get_qr_access, set_user_premium, admin_get_all_users, admin_ban_user, admin_get_all_qr
 
 # UTILISATIONS UNIT TESTS
 
@@ -18,6 +19,11 @@ def test_2_generate_qr():
     img = generate_qr('https://www.itu.edu.tr/', 5, 124, 'itu_logo.jpg')
     histogram = img.histogram()
     assert histogram == [5796, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 2, 4, 0, 2, 4, 5, 2, 2, 1, 5, 4, 2, 2, 1, 2, 1, 2, 3, 7, 2, 4, 2, 5, 2, 1, 2, 1, 2, 3, 2, 1, 2, 2, 0, 4, 4, 2, 0, 3, 2, 2, 1, 3, 5, 3, 0, 7, 4, 1, 5, 1, 3, 3, 5, 7, 0, 1, 8, 5, 5, 7, 5, 9399, 5796, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 2, 4, 1, 3, 3, 5, 2, 2, 4, 3, 2, 3, 2, 0, 2, 3, 2, 2, 8, 3, 4, 5, 2, 2, 3, 2, 3, 4, 6, 5, 1, 4, 4, 2, 5, 4, 2, 1, 4, 1, 2, 2, 0, 1, 2, 2, 1, 1, 1, 1, 6, 0, 4, 8, 1, 4, 5, 7, 2, 4, 6, 9397, 5796, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 3, 2, 1, 0, 1, 2, 1, 0, 0, 1, 0, 0, 3, 0, 0, 0, 2, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 5, 6, 4, 8, 2, 6, 6, 1, 1, 7, 3, 10, 7, 4, 1, 4, 3, 5, 2, 2, 11, 6, 2, 2, 0, 5, 2, 3, 2, 1, 2, 6, 4, 7, 7, 6, 9399]
+
+def test_3_generate_qr():
+    img = generate_qr('https://www.itu.edu.tr/', 1, 512)
+    histogram = img.histogram()
+    assert histogram == [106879, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 155265, 106879, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 155265, 106879, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 155265]
 
 def test_timestamp():
     assert timestamp() == datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -58,6 +64,11 @@ def test_3_insert_new_qr(mocker):
     user = User(id='123',username='username',hashed_password='hashed_password',qr_count=0)
     mocker.patch('logic_layer.insert_qr_to_db', return_value=False)
     assert insert_new_qr('link',user) == 2
+
+def test_get_user_qr(mocker):
+    user = User(id='123',username='username',hashed_password='hashed_password')
+    mocker.patch('logic_layer.get_qr_by_user_id', return_value=[['0','0','0','0','0','0']])
+    assert get_user_qr(user) == [{'qr_id': '0', 'link': '0'}]
 
 def test_generate_qr_for_user(mocker):
     user = User(id='123',username='username',hashed_password='hashed_password')
@@ -128,6 +139,12 @@ def test_get_qr_access(mocker):
     user = User(id='123',username='username',hashed_password='hashed_password')
     mocker.patch('logic_layer.get_qr_by_qr_and_user_id', return_value=False)
     assert get_qr_access(123,user) == 1
+
+def test_2_get_qr_access(mocker):
+    user = User(id='123',username='username',hashed_password='hashed_password',is_admin=True)
+    mocker.patch('logic_layer.get_qr_by_qr_and_user_id', return_value=True)
+    mocker.patch('logic_layer.get_qr_history_from_db', return_value=[['0','0','0','0','0','0']])
+    assert get_qr_access(123,user) == [{'time': '0', 'country': '0', 'city': '0', 'agent': '0', 'latitude': '0', 'longitude': '0'}]
 
 def test_set_user_premium(mocker):
     user = User(id='123',username='username',hashed_password='hashed_password')
